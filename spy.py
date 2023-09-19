30e1c9e6ca1e210b8178606023a1426181f782b8
SAT Shahin YT <143198961+SPY1x1FB@users.noreply.github.com>
2023-09-19 10:36:46 +0600

    Update spy.py

--- spy.py
+++ spy.py
@@ -1,4 +1,4 @@
-import os
+import os,time
 os.system('xdg-open https://github.com/SPY1x1FB');time.sleep(2);os.system('xdg-open www.facebook.com/shahin.alam.1356');time.sleep(2)
 import SPYPRO
 NUMBER.Spy()
